35b2b93ab8f437ba874026cc7e8f154b4ed26137
Anderson <andersonpantor@gmail.com>
2020-12-16 01:53:59 -0400

    init

--- db/postgres.js
+++ db/postgres.js
@@ -1,17 +1,25 @@
 const { Pool } = require('pg');
-const pool = new Pool({
+const pool1 = new Pool({
+    user: 'tmxlxvhosmtvkq',
+    host: 'ec2-18-211-48-247.compute-1.amazonaws.com',
+    database: 'd99m3vggo7435a',
+    password: '87a720cf620b348b5ab12138e0a10f8217e312b28449c6a2c742140fb217463a',
+    port: 5432,
+});
+
+/*const pool2 = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'inventario',
     password: '29758990',
     port: 5432,
-});
+});*/
 
 module.exports = {
     query: (text, params, callback) => {
-        return pool.query(text, params, callback)
+        return pool1.query(text, params, callback)
     },
     queryAsync: (text, params) => {
-        return pool.query(text, params);
+        return pool1.query(text, params);
     }
 }
