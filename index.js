666eef97cb1da7465fe588ef8119c7d3905c48ee
Anderson <andersonpantor@gmail.com>
2020-12-18 10:17:17 -0400

    Clon De Twitter

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(session({secret:'justdanceSecret', saveUninitialized:true, resave:true}));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+    res.setHeader("Access-Control-Allow-Origin", "https://localhost:5000");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Option, Option2");
     res.setHeader("Access-Control-Allow-Credentials", "true");
